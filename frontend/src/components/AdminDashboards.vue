c024d727bf2160fee120c82554d6a4b078c4737e
Andres Elacion III <andreselacioniii@gmail.com>
2024-04-06 17:37:30 +0800

    Implement ui for dashboard card

--- frontend/src/components/AdminDashboards.vue
+++ frontend/src/components/AdminDashboards.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import WelcomeBanner from '../components/WelcomeBanner.vue'
+import OpenTicket from '../components/dashboard/OpenTicket.vue'
+import ResolvedTicket from '../components/dashboard/ResolvedTicket.vue'
+import UserCount from '../components/dashboard/UserCount.vue'
+import SupportCount from '../components/dashboard/SupportCount.vue'
+import AdminCount from '../components/dashboard/AdminCount.vue'
 </script>
 <template>
   <!-- Main -->
@@ -8,28 +13,21 @@ import WelcomeBanner from '../components/WelcomeBanner.vue'
     <div class="w-[90%] ml-[70px] mt-10">
       <WelcomeBanner />
     </div>
-    <div class="h-[calc(100vh-50px)]">
-      <div class="flex justify-center my-[100px]">
-        <form class="w-full max-w-lg">
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-first-name"
-              >
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane"
-              />
-              <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-            </div>
-          </div>
-        </form>
+    <main>
+      <div class="px-4 sm:px-6 lg:px-8 py-2 w-full mx-auto">
+  
+        <!-- Cards -->
+        <div class="grid grid-cols-12 gap-6">
+        
+          <!-- Total Open Ticket -->
+          <OpenTicket />
+          <ResolvedTicket />
+          <UserCount />
+          <SupportCount />
+          <AdminCount />
+        </div>
       </div>
-    </div>
+    </main>
   </div>
   <!-- Main -->
 </template>
